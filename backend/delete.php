c829d1c95edb895750d0a8527962037f46b349d7
RivaldiDev <57316177+RivaldiDev@users.noreply.github.com>
2023-11-13 22:44:01 +0700

    Update delete.php

--- backend/delete.php
+++ backend/delete.php
@@ -10,9 +10,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
     $result = mysqli_query($db_connect, "DELETE FROM products WHERE id = $productId");
 
     if ($result) {
-        echo "Product deleted successfully.";
+        echo "Produk berhasil di hapus.";
     } else {
-        echo "Error deleting product.";
+        echo "Produk gagal di hapus.";
     }
 } else {
     echo "Invalid request.";
